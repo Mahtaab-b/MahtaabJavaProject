5cff6de9daad312b576e453aa3fc4df55e7d6c17
mahtaabbrar <mahtaabbrar@localhost>
2022-08-02 16:55:55 -0600

    Adding bank class

---
+++ src/application/Bank.java
@@ -0,0 +1,65 @@
+package application;
+
+import java.util.ArrayList;
+
+public class Bank {
+	
+	private ArrayList <Account> bank = new ArrayList<Account>();
+
+	public Bank() {
+		setBank(new ArrayList<Account>());
+	}
+	
+	public ArrayList <Account> getBank() {
+		return bank;
+	}
+
+	public void setBank(ArrayList <Account> bank) {
+		this.bank = bank;
+	}
+	
+	public void addBankAccounts(Account a) {
+		bank.add(a);
+	}
+	
+	public int searchIfAccountExists(String accountNumber) {
+
+		int exists = 0;
+
+		int index = 0;
+
+		while (index < getBank().size()) {
+
+			if (accountNumber.equals(getBank().get(index).getAccountNumber())) {
+				exists += 1;
+				break;
+			}
+
+			index++;
+		}
+
+		return exists;
+
+	}
+
+	public Account accountSaver(String accountNumber) {
+
+		Account takeThisAccount = new Account();
+
+		int index = 0;
+
+		while (index < getBank().size()) {
+
+			if (accountNumber.equals(getBank().get(index).getAccountNumber())) {
+				takeThisAccount = getBank().get(index);
+				break;
+			}
+
+			index++;
+		}
+
+		return takeThisAccount;
+	}
+	
+
+}
